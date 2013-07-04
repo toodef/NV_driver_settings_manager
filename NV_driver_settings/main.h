819388d7fe0b32e2ef18776f7c334052a1146b78
Fedotov Anton <fedotov.anton.af@gmail.com>
2013-07-04 16:46:35 +0400

    std::map -> boost::bimap

--- NV_driver_settings/main.h
+++ NV_driver_settings/main.h
@@ -2,15 +2,18 @@
 
 #pragma warning(disable : 4996)
 #pragma warning(disable : 4146)
+#pragma warning(disable : 4103)
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #include <nvapi.h>
 #include <NvApiDriverSettings.h>
 #pragma comment(lib, "nvapi.lib")
 
 #include <boost/format.hpp>
+#include <boost/bimap.hpp>
 
 #include <yaml.h>
 #pragma comment(lib, "yaml.lib")
