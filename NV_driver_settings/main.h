a478b5e067ed6412dc9cb482b0264b331c486ef4
Fedotov Anton <fedotov.anton.af@gmail.com>
2013-07-04 19:51:11 +0400

    temp commit

--- NV_driver_settings/main.h
+++ NV_driver_settings/main.h
@@ -23,3 +23,40 @@ using namespace boost;
 using namespace YAML;
 
 #include "nv_api.h"
+
+// temp
+__forceinline NvAPI_UnicodeString & string_to_NvUS( const string & str )
+{
+   size_t len = str.length();
+
+   static NvAPI_UnicodeString us = {0};
+
+   for (size_t i = 0; i < len + 1; ++i)
+      us[i] = (NvU16)(str.c_str()[i]);
+
+   return us;
+}
+
+__forceinline string NvUS_to_string( const NvU16 * str )
+{
+   string new_string;
+
+   unsigned int i = 0;
+
+   while (str[i])
+      new_string += (char)str[i], i++;
+
+   return new_string;
+}
+
+__forceinline string NvUS_to_string( const NvAPI_UnicodeString * str )
+{
+   string new_string;
+
+   unsigned int i = 0;
+
+   while (str[i])
+      new_string += (char)str[i], i++;
+
+   return new_string;
+}
