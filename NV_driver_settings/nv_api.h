771fce37a6fc117ab61bc7d981e6c3e347b22716
Fedotov Anton <fedotov.anton.af@gmail.com>
2013-07-04 16:46:17 +0400

    std::map -> boost::bimap

--- NV_driver_settings/nv_api.h
+++ NV_driver_settings/nv_api.h
@@ -7,6 +7,8 @@
 using boost::assign::map_list_of;
 using boost::assign::list_of;
 
+using namespace boost::bimaps;
+
 class nv_api
 {
 public:
@@ -18,17 +20,24 @@ public:
    bool change_setting ( Node const & setting );
    bool change_settings( Node const & settings );
 
+   void load_settings_from_file( const string & file_name ); // from file do driver
+   void save_settings_to_file  ( const string & file_name ); // from driver to file
+
 private:
    void init_map();
 
    unsigned int get_value_id_from_setting_id( NvU32 id, std::string const & value_name );
 
-   NvDRSSessionHandle session_;
-   NvDRSProfileHandle profile_;
+   NvDRSSessionHandle session_  ;
+   NvDRSProfileHandle profile_  ;
+   NVDRS_PROFILE      prof_info_;
 
-   typedef map<NvU32, map<string, unsigned int>> dword_map_t ;
-   typedef map<NvU32, list<string>>              string_map_t;
 
-   dword_map_t  dwrd_setting_map_;
-   string_map_t str_setting_map_ ;
+   typedef map<NvU32, bimap<string, unsigned int>> dword_map_t;
+   typedef map<NvU32, list<string>>                string_map_t ;
+
+   dword_map_t     dwrd_setting_map_;
+   string_map_t    str_setting_map_ ;
+
+   typedef vector<NVDRS_SETTING> settings_t;
 };
